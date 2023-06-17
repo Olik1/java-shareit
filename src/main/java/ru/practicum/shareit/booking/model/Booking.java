bbfcf4dae3f4ad3745c99e6291e0f114959370e3
Olga <110121612+Olik1@users.noreply.github.com>
2023-06-17 14:08:29 +0300

    model booking

---
+++ src/main/java/ru/practicum/shareit/booking/model/Booking.java
@@ -0,0 +1,28 @@
+package ru.practicum.shareit.booking.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import ru.practicum.shareit.item.model.Item;
+import ru.practicum.shareit.user.model.User;
+
+import java.time.LocalDateTime;
+
+/**
+ * TODO Sprint add-bookings.
+ */
+@Data
+@RequiredArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Booking {
+    private Long id;
+    private LocalDateTime start;
+    private LocalDateTime end;
+    private Item item;
+    private User booker;
+    private Status status;
+
+
+}
