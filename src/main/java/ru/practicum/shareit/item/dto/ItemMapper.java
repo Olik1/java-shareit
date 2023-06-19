26963809ccae51d853c7b4d36089a3d6da7085b8
Olga <110121612+Olik1@users.noreply.github.com>
2023-06-19 11:15:21 +0300

    fix: add annotation
     for final utitlity class

--- src/main/java/ru/practicum/shareit/item/dto/ItemMapper.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemMapper.java
@@ -1,7 +1,8 @@
 package ru.practicum.shareit.item.dto;
 
+import lombok.experimental.UtilityClass;
 import ru.practicum.shareit.item.model.Item;
-
+@UtilityClass
 public class ItemMapper {
     public static ItemDto toItemDto(Item item) { // Item -> в объект ItemDto
         return ItemDto.builder()
--- src/main/java/ru/practicum/shareit/user/dto/UserMapper.java
+++ src/main/java/ru/practicum/shareit/user/dto/UserMapper.java
@@ -1,7 +1,8 @@
 package ru.practicum.shareit.user.dto;
 
+import lombok.experimental.UtilityClass;
 import ru.practicum.shareit.user.model.User;
-
+@UtilityClass
 public class UserMapper {
     public static UserDto toUserDto(User user) {
         return UserDto.builder()
