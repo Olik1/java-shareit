8f4e89769108d3de2894ff2e67a4d72b902ad1fc
Olik13 <dorofeeva86@yandex.ru>
2023-07-25 15:20:31 +0300

    add new branch

--- src/main/java/ru/practicum/shareit/exception/ConflictException.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.shareit.exception;
-
-public class ConflictException extends RuntimeException {
-    public ConflictException(String message) {
-        super(message);
-    }
-}
--- src/main/java/ru/practicum/shareit/exception/WrongStatusException.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.shareit.exception;
-
-public class WrongStatusException extends RuntimeException {
-    public WrongStatusException(String message) {
-        super(message);
-    }
-}
